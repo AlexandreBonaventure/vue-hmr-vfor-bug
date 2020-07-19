c19acd4f538d2b21fc0d386695cebd9b38b9df21
Alexandre Geissmann <alex@la-bonaventure.com>
2020-07-18 20:44:22 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <ItemComponent
+    v-for="(item, i) in items"
+    :item="item"
+    :key="item.id"
+  />
+</template>
+
+<script>
+import ItemComponent from './components/ItemComponent.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    ItemComponent
+  },
+  setup() {
+    const items = ref(null);
+    setTimeout(() => {
+      items.value = [{ id: 0 }, { id: 1 }] 
+    }, 100)
+    setTimeout(() => {
+      items.value = [{ id: 0 }, { id: 1 }]
+    }, 200)
+    return {
+      items
+    }
+  }
+}
+</script>
---
+++ src/components/ItemComponent.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    item: {
+      required: true
+    }
+  }
+});
+</script>
+
+<template>
+  <div>
+    <div>change my text here for triggering HMR: HERE</div>
+    <div>{{ item }}</div>
+  </div>
+</template>
